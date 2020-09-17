ca4258d0783a8868e1739f12059262911b8b8e39
Igor Dudek <dudekigor@gmail.com>
2020-09-17 16:06:06 +0200

    Added alert

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -1,11 +1,19 @@
 const path = require("path");
 const osu = require("node-os-utils");
+const { ipcRenderer } = require("electron");
 const cpu = osu.cpu;
 const mem = osu.mem;
 const os = osu.os;
 
-let cpuOverload = 80;
-let alertFrequency = 1;
+let cpuOverload;
+let alertFrequency;
+
+//Get settings & values
+
+ipcRenderer.on("settings:get", (e, settings) => {
+  cpuOverload = +settings.cpuOverload;
+  alertFrequency = +settings.alertFrequency;
+});
 
 // Run every 2s
 setInterval(() => {
